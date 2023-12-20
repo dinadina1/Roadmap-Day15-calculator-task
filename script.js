b2a50dba6b96655c4ec18c3ecc05b1be966ba45b
dinadina1 <dinad9355@gmail.com>
2023-12-20 10:50:05 +0530

    Last commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 var main=document.createElement("main")
 main.setAttribute("class","container")
-main.innerHTML="<h1>Calculator</h1>"
+main.innerHTML="<h1 id='title'><p id='description'>Calculator</p></h1>"
 var table=document.createElement("table")
 table.setAttribute("class","table")
 table.setAttribute("id","calcu");
@@ -13,23 +13,23 @@ tr1.innerHTML="<td colspan=4><input type=text name=result id=result></td>"
 table.append(tr1);
 
 var tr2=document.createElement("tr");
-tr2.innerHTML="  <td><input type=button value=C onclick=clr()></td>     <td><input type=button value=/ onclick=display('/') onkeydown=myFunction(event)></td>         <td><input type=button value=* onclick=display('*') onkeydown=myFunction(event)></td>        <td><input type=button value=&lt; onclick=back()></td>"
+tr2.innerHTML="  <td><button type=button onclick=clr() id='clear'>C</button></td>     <td><button type=button onclick=display('/') onkeydown=myFunction(event) id='divide'>/</button></td>         <td><button type=button onclick=display('*') onkeydown=myFunction(event) id='multiplication'>*</button></td>        <td><button type=button onclick=back() id='back'>&lt</button></td>"
 table.append(tr2);
 
 var tr3=document.createElement("tr");
-tr3.innerHTML=" <td><input type=button value=7 onclick=display('7') onkeydown=myFunction(event)></td>   <td><input type=button value=8 onclick=display('8') onkeydown=myFunction(event)></td>   <td><input type=button value=9 onclick=display('9') onkeydown=myFunction(event)></td>   <td><input type=button value='- '  onclick=display('-') onkeydown=myFunction(event)></td>"
+tr3.innerHTML=" <td><button type=button onclick=display('7') onkeydown=myFunction(event) id='7'>7</button></td>   <td><button type=button onclick=display('8') onkeydown=myFunction(event) id='8'>8</button></td>   <td><button type=button onclick=display('9') onkeydown=myFunction(event) id='9'>9</button></td>   <td><button type=button onclick=display('-') onkeydown=myFunction(event) id='subtract'>-</button></td>"
 table.append(tr3);
 
 var tr4=document.createElement("tr");
-tr4.innerHTML=" <td><input type=button value=4 onclick=display('4') onkeydown=myFunction(event)></td>   <td><input type=button value=5 onclick=display('5') onkeydown=myFunction(event)></td>   <td><input type=button value=6 onclick=display('6') onkeydown=myFunction(event)></td>   <td><input type=button value=+ onclick=display('+') onkeydown=myFunction(event)></td>"
+tr4.innerHTML=" <td><button type=button onclick=display('4') onkeydown=myFunction(event) id='4'>4</button></td>   <td><button type=button onclick=display('5') onkeydown=myFunction(event) id='5'>5</button></td>   <td><button type=button onclick=display('6') onkeydown=myFunction(event) id='6'>6</button></td>   <td><button type=button onclick=display('+') onkeydown=myFunction(event) id='add'>+</button></td>"
 table.append(tr4);
 
 var tr5=document.createElement("tr");
-tr5.innerHTML="<td><input type=button value=1 onclick=display('1') onkeydown=myFunction(event)></td>    <td><input type=button value=2 onclick=display('2') onkeydown=myFunction(event)></td>   <td><input type=button value=3 onclick=display('3') onkeydown=myFunction(event)></td>   <td><input type=button value='. '  onclick=display('.') onkeydown=myFunction(event)></td>"
+tr5.innerHTML="<td><button type=button onclick=display('1') onkeydown=myFunction(event) id='1'>1</button></td>    <td><button type=button onclick=display('2') onkeydown=myFunction(event) id='2'>2</button></td>   <td><button type=button onclick=display('3') onkeydown=myFunction(event) id='3'>3</button></td>   <td><button type=button  onclick=display('.') onkeydown=myFunction(event) id='dot'>.</button></td>"
 table.append(tr5);
 
 var tr6=document.createElement("tr");
-tr6.innerHTML="<td><input type=button value=% onclick=display('%') onkeydown=myFunction(event)></td> <td><input type=button value=0 onclick=display('0') onkeydown=myFunction(event)></td>   <td><input type=button value=00 style=width: 90px; onclick=display('00') onkeydown=myFunction(event)></td> <td><input type=button value== onclick=solve()></td>"
+tr6.innerHTML="<td><button type=button onclick=display('%') onkeydown=myFunction(event) id='modulas'>%</button></td> <td><button type=button onclick=display('0') onkeydown=myFunction(event) id='0'>0</button></td>   <td><button type=button style=width: 90px; onclick=display('00') onkeydown=myFunction(event) id='00'>00</button></td> <td><button type=button onclick=solve() id='equal'>=</button></td>"
 table.append(tr6);
 
 document.body.append(main)
